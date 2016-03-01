394ac49ece4ce50dea02e759f6c0b2f34accb0b7
unionsoftware <ggmortum@hotmail.com>
2016-02-29 23:09:19 -0500

    update_usr

--- run.sh
+++ run.sh
@@ -5,6 +5,6 @@ echo "Add all files"
 git add .
 git commit -m $1
 git remote set-url  origin https://github.com/unionsoftware/Simulation
-git push -f origin master | echo "unionsoftware"
+git push -f origin master | `echo "unionsoftware"`
 
 echo "Finished."
